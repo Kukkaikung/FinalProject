a58a884f6b82dbcfb28508e3f49d478dfe8a681f
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-03-02 23:50:19 +0700

    read csv airthai_02

---
+++ data_mange/clean.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df_airthai_02 = pd.read_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\source\\air4thai_02t_2024-02-27_2024-02-27.csv")
+print(df_airthai_02)
