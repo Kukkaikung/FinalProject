cc6eb6b9b630f37043ecbf69746e23c75c59e6aa
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-03-03 00:00:17 +0700

    clean data airthai_03

--- data_mange/clean.py
+++ data_mange/clean.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
+
+'''
 df_airthai_02 = pd.read_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\source\\air4thai_02t_2024-02-27_2024-02-27.csv")
 #print(df_airthai_02)
 clean_df_airthai_02 = df_airthai_02.dropna(axis=1)
 #print(clean_df_airthai_02)
 clean_df_airthai_02.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean_airthai_02.csv")
+'''
+
+df_airthai_03 = pd.read_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\source\\air4thai_03t_2024-02-27_2024-02-27.csv")
+#print(df_airthai_03)
+clean_df_airthai_03 = df_airthai_03.dropna(axis=1)
+#print(clean_df_airthai_03)
+clean_df_airthai_03.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean_airthai_03.csv")
