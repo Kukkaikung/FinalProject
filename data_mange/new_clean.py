507dc2d5663eb6e3fb1d69a3fb415ed8e914f407
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-03-04 23:47:01 +0700

    update comment

--- data_mange/new_clean.py
+++ data_mange/new_clean.py
@@ -3,7 +3,9 @@ import pandas as pd
 df_airthai = pd.read_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\source\\air4thai_44t_2023-01-01_2024-02-27.csv")
 print(df_airthai)
 
+#01 คลีนแล้วยังไม่ละเอียด
 clean_data01 = df_airthai.dropna(axis=1)
+#02 คลีนละเอียดกว่า 01 
 clean_data02 = df_airthai.dropna(axis=1, thresh=5)
 
 clean_data01.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean01_air4thai_44t_2023-01-01_2024-02-27.csv")
