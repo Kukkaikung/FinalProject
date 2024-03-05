03c04e7b0c879a6d3f74d5ebe05c04a81b2b408f
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-03-05 23:02:51 +0700

    delete useless data and clean finish clean data

--- data_mange/new_clean.py
+++ data_mange/new_clean.py
@@ -1,15 +1,16 @@
 import pandas as pd
 
 df_airthai = pd.read_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\source\\air4thai_44t_2023-01-01_2024-02-27.csv")
-print(df_airthai)
 
-#01 คลีนแล้วยังไม่ละเอียด
-clean_data01 = df_airthai.dropna(axis=1)
-#02 คลีนละเอียดกว่า 01 
-clean_data02 = df_airthai.dropna(axis=1, thresh=5)
 
-clean_data01.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean01_air4thai_44t_2023-01-01_2024-02-27.csv")
+clean_data02 = df_airthai.dropna(axis=1, thresh=1)
+
+not_number_columns = ['DATETIMEDATA', 'stationID']
+number_columns = clean_data02.columns.difference(not_number_columns)
+clean_data02[number_columns] = clean_data02[number_columns].fillna(clean_data02[number_columns].mean())
+
 clean_data02.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean02_air4thai_44t_2023-01-01_2024-02-27.csv")
+
 
 
 
