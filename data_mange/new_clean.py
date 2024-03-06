42a5c7042dc017f2c6068abdd7a9195d2f896122
Duriya Gosonhemmanee <duriya.0577@gmail.com>
2024-03-06 11:23:44 +0700

    update cleaned data

--- data_mange/new_clean.py
+++ data_mange/new_clean.py
@@ -9,6 +9,9 @@ not_number_columns = ['DATETIMEDATA', 'stationID']
 number_columns = clean_data02.columns.difference(not_number_columns)
 clean_data02[number_columns] = clean_data02[number_columns].fillna(clean_data02[number_columns].mean())
 
+clean_data02['TEMP'].replace(0, clean_data02['TEMP'].mean(), inplace=True)
+clean_data02['WS'].replace(0, clean_data02['WS'].mean().round(), inplace=True)
+
 clean_data02.to_csv("C:\\Users\\ASUS\\Desktop\\VS code\\PsuTerm02\\241-152\\final_project\\data_mange\\clean02_air4thai_44t_2023-01-01_2024-02-27.csv")
 
 
