5927e9cf32e0e4bc59a80080e4f0ff020ebf09df
อิทธิพัทธ์ จันทร์ขุน <phat4725@gmail.com>
2024-03-10 21:45:48 +0700

    ทดลองใส่ข้อมูล

--- webpanan/app.py
+++ webpanan/app.py
@@ -1,53 +1,19 @@
-import dash
-import dash_bootstrap_components as dbc
-from dash import html, dcc, Input, Output
+# Import packages
+from dash import Dash, html, dash_table
+import pandas as pd
 
-import datetime
+# Incorporate data
+df = pd.read_csv('predict_data/predictions_PM25.csv')
 
-import pandas
-import plotly.express as px
+# Initialize the app
+app = Dash(__name__)
 
-app = dash.Dash(external_stylesheets=[dbc.themes.DARKLY])
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+])
 
-
-
-def get_current_time(n_intervals):
-    now = datetime.datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M:%S")
-
-
-current_time_interval = dcc.Interval(
-    id="current-time-interval", interval=1000, n_intervals=0
-)
-
-app.layout = html.Div(
-    children=[
-        dbc.Row(
-            children=[
-                dbc.Col(
-                    children=[
-                        html.H1(children="Hello Dash"),
-                        html.Div(
-                            children="""
-        Dash: A web application framework for your data.
-    """
-                        ),
-                    ]
-                ),
-                dbc.Col(
-                    children=[
-                        html.H1(children="Time"),
-                        html.H2(id="current-time"),
-                    ]
-                ),
-            ]
-        ),
-        current_time_interval,
-    ]
-)
-
-
-# app.layout = dbc.Alert("Hello, Bootstrap!", className="m-5")
-
-if __name__ == "__main__":
-    app.run_server(debug=True)  
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
